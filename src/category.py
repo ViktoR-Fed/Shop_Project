4c0743f9153eb0b22dc8967b0a4b674d1eb159f0
Виктор <20viktor120@gmail.com>
2026-02-11 18:40:32 +0300

    исключения

--- src/category.py
+++ src/category.py
@@ -1,3 +1,4 @@
+from src.exceptions import ZeroQuantityProduct
 from src.product import Product
 
 
@@ -24,8 +25,17 @@ class Category:
     def add_product(self, product: Product):
         """Метод для добавления продуктов"""
         if isinstance(product, Product):
-            self.__products.append(product)
-            Category.product_count += 1
+            try:
+                if product.quantity == 0:
+                    raise ZeroQuantityProduct("Нельзя задать продукт с нулевым количеством")
+            except ZeroQuantityProduct as e:
+                print(str(e))
+            else:
+                self.__products.append(product)
+                Category.product_count += 1
+                print("Продукт успешно добавлен")
+            finally:
+                print("Обработка добавления продукта завершена")
         else:
             raise TypeError
 
@@ -41,3 +51,11 @@ class Category:
     @property
     def products_in_list(self):
         return self.__products
+
+    def middle_price(self):
+        try:
+            result = sum((product.price for product in self.__products)) / len(self.__products)
+        except ZeroDivisionError:
+            return 0
+        else:
+            return result
